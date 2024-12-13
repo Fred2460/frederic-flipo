cf3363d4fa2ec4ed4c00072538352e4b9468c18b
Frederic FLIPO <fred.web24@gmail.com>
2024-12-13 15:21:38 +0100

    maj lien site web

--- src/components/LocalData.jsx
+++ src/components/LocalData.jsx
@@ -9,8 +9,8 @@ const LocalData = () => {
     "name": "Frederic Flipo",
     "image": "./assets/image_github_ffl.png",
     "description": "Développeur freelance proposant des services de développement et d'optimisation de sites web",
-    "@id": "https://frederic.flipo.github.io/",
-    "url": "https://frederic.flipo.github.io/",
+    "@id": "https://fred2460.github.io/fred.flipo",
+    "url": "https://fred2460.github.io/fred.flipo",
     "telephone": "+33 6 82 86 01 60",
     "address": {
       "@type": "PostalAddress",
